085c064367e93d4b6adf24fe332ee54c2437103c
Otavio Lemos <otaviolemos@gmail.com>
2020-12-20 14:09:00 -0300

    test: create note with invalid title

--- src/entities/note.spec.ts
+++ src/entities/note.spec.ts
@@ -1,5 +1,7 @@
 import { User } from './user'
 import { Note } from './note'
+import { InvalidTitleError } from './errors/invalid-title-error'
+import { left } from '../shared/either'
 
 describe('Note entity', () => {
   test('should be created with a valid title and owner', () => {
@@ -9,5 +11,12 @@ describe('Note entity', () => {
     const note: Note = Note.create(validOwner, validTitle).value as Note
     expect(note.title.value).toEqual('my note')
     expect(note.owner.email.value).toEqual('my@mail.com')
+  })
+
+  test('should not be created with invalid title', () => {
+    const invalidTitle = ''
+    const validEmail = 'my@mail.com'
+    const validOwner: User = User.create({ email: validEmail }).value as User
+    expect(Note.create(validOwner, invalidTitle)).toEqual(left(new InvalidTitleError()))
   })
 })
