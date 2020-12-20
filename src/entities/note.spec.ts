4225330daeca9ace28e7a84fa5e15281101caf07
Otavio Lemos <otaviolemos@gmail.com>
2020-12-20 14:17:20 -0300

    test: add content field to note

--- src/entities/note.spec.ts
+++ src/entities/note.spec.ts
@@ -7,16 +7,19 @@ describe('Note entity', () => {
   test('should be created with a valid title and owner', () => {
     const validTitle = 'my note'
     const validEmail = 'my@mail.com'
+    const validContent = 'content'
     const validOwner: User = User.create({ email: validEmail }).value as User
-    const note: Note = Note.create(validOwner, validTitle).value as Note
+    const note: Note = Note.create(validOwner, validTitle, validContent).value as Note
     expect(note.title.value).toEqual('my note')
     expect(note.owner.email.value).toEqual('my@mail.com')
+    expect(note.content).toEqual(validContent)
   })
 
   test('should not be created with invalid title', () => {
     const invalidTitle = ''
     const validEmail = 'my@mail.com'
+    const validContent = 'content'
     const validOwner: User = User.create({ email: validEmail }).value as User
-    expect(Note.create(validOwner, invalidTitle)).toEqual(left(new InvalidTitleError()))
+    expect(Note.create(validOwner, invalidTitle, validContent)).toEqual(left(new InvalidTitleError()))
   })
 })
