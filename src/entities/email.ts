3d75ed449de6a16c124ed08aa4a317d5faca4465
Otavio Lemos <otaviolemos@gmail.com>
2020-12-20 14:24:06 -0300

    fix: include email in call to invalid email constructor

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -13,7 +13,7 @@ export class Email {
       return right(new Email(email))
     }
 
-    return left(new InvalidEmailError())
+    return left(new InvalidEmailError(email))
   }
 
   static validate (email: string): boolean {
