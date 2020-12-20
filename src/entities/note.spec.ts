e4f63a7a58d399a5b03d07142c9fd545eef71d44
Otavio Lemos <otaviolemos@gmail.com>
2020-12-20 14:06:40 -0300

    test: create valid note

--- src/entities/note.spec.ts
+++ src/entities/note.spec.ts
@@ -1,1 +1,13 @@
+import { User } from './user'
+import { Note } from './note'
 
+describe('Note entity', () => {
+  test('should be created with a valid title and owner', () => {
+    const validTitle = 'my note'
+    const validEmail = 'my@mail.com'
+    const validOwner: User = User.create({ email: validEmail }).value as User
+    const note: Note = Note.create(validOwner, validTitle).value as Note
+    expect(note.title.value).toEqual('my note')
+    expect(note.owner.email.value).toEqual('my@mail.com')
+  })
+})
