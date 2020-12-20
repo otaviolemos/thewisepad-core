70b45f1d14b7203865cb64998b76d2568297d8d9
Otavio Lemos <otaviolemos@gmail.com>
2020-12-20 15:13:12 -0300

    test: invalidate passwords without number

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,13 +1,21 @@
 import { User } from './user'
 import { left } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidPasswordError } from './errors/invalid-password-error'
 
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
     const invalidEmail = 'invalid_email'
     const validPassword = '1validpassword'
     const error = User.create({ email: invalidEmail, password: validPassword })
-    expect(error).toEqual(left(new InvalidEmailError('invalid_email')))
+    expect(error).toEqual(left(new InvalidEmailError(invalidEmail)))
+  })
+
+  test('should not create user with invalid password (no numbers)', () => {
+    const validEmail = 'any@mail.com'
+    const invalidPassword = 'invalid'
+    const error = User.create({ email: validEmail, password: invalidPassword })
+    expect(error).toEqual(left(new InvalidPasswordError()))
   })
 
   test('should create user with valid data', () => {
