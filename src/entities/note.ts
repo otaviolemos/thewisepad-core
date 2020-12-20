e5946b2baddebf5d845471c83a9929f218e1cb0e
Otavio Lemos <otaviolemos@gmail.com>
2020-12-20 14:17:07 -0300

    feat: add content field to note

--- src/entities/note.ts
+++ src/entities/note.ts
@@ -6,10 +6,12 @@ import { Either, left, right } from '../shared/either'
 export class Note {
   private readonly _title: Title
   private readonly _owner: User
+  private readonly _content: string
 
-  constructor (owner: User, title: Title) {
+  constructor (owner: User, title: Title, content: string) {
     this._owner = owner
     this._title = title
+    this._content = content
   }
 
   get title () {
@@ -20,11 +22,15 @@ export class Note {
     return this._owner
   }
 
-  static create (owner: User, title: string): Either<InvalidTitleError, Note> {
+  get content () {
+    return this._content
+  }
+
+  static create (owner: User, title: string, content: string): Either<InvalidTitleError, Note> {
     const titleOrError = Title.create(title)
     if (titleOrError.isLeft()) {
       return left(titleOrError.value)
     }
-    return right(new Note(owner, titleOrError.value))
+    return right(new Note(owner, titleOrError.value, content))
   }
 }
