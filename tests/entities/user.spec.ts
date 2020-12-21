a16062a84f2350773e374071866a6ea88a806068
Otavio Lemos <otaviolemos@gmail.com>
2020-12-21 11:13:01 -0300

    refactor: make frequently used variables global

--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -3,31 +3,29 @@ import { left } from '../../src/shared/either'
 import { InvalidEmailError } from '../../src/entities/errors/invalid-email-error'
 import { InvalidPasswordError } from '../../src/entities/errors/invalid-password-error'
 
+const invalidEmail = 'invalid_email'
+const validPassword = '1validpassword'
+const validEmail = 'any@mail.com'
+const invalidPasswordWithNoNumbers = 'invalid'
+const invalidPasswordWithTooFewCharacters = '123ab'
+
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
-    const invalidEmail = 'invalid_email'
-    const validPassword = '1validpassword'
     const error = User.create({ email: invalidEmail, password: validPassword })
     expect(error).toEqual(left(new InvalidEmailError(invalidEmail)))
   })
 
   test('should not create user with invalid password (no numbers)', () => {
-    const validEmail = 'any@mail.com'
-    const invalidPassword = 'invalid'
-    const error = User.create({ email: validEmail, password: invalidPassword })
+    const error = User.create({ email: validEmail, password: invalidPasswordWithNoNumbers })
     expect(error).toEqual(left(new InvalidPasswordError()))
   })
 
   test('should not create user with invalid password (too few chars)', () => {
-    const validEmail = 'any@mail.com'
-    const invalidPassword = '123ab'
-    const error = User.create({ email: validEmail, password: invalidPassword })
+    const error = User.create({ email: validEmail, password: invalidPasswordWithTooFewCharacters })
     expect(error).toEqual(left(new InvalidPasswordError()))
   })
 
   test('should create user with valid data', () => {
-    const validEmail = 'any@mail.com'
-    const validPassword = '1validpassword'
     const user: User = User.create({ email: validEmail, password: validPassword }).value as User
     expect(user.email.value).toEqual(validEmail)
     expect(user.password.value).toEqual(validPassword)
