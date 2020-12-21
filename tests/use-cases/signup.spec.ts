986eeb1862cff023068361f5a014e1bd0651d712
Otavio Lemos <otaviolemos@gmail.com>
2020-12-21 11:55:32 -0300

    test: prevent signup to register existing user

--- tests/use-cases/signup.spec.ts
+++ tests/use-cases/signup.spec.ts
@@ -4,19 +4,28 @@ import { InMemoryUserRepository } from './in-memory-user-repository'
 import { Encoder } from '../../src/use-cases/signup/ports/encoder'
 import { FakeEncoder } from './signup/fake-encoder'
 import { Signup } from '../../src/use-cases/signup/signup'
+import { ExistingUserError } from '../../src/use-cases/signup/errors/existing-user-error'
 
 const validEmail = 'any@mail.com'
 const validPassword = '1validpassword'
-const userSignupRequest: UserData = { email: validEmail, password: validPassword }
-const userRepository: UserRepository = new InMemoryUserRepository([])
+const validUserSignupRequest: UserData = { email: validEmail, password: validPassword }
+const emptyUserRepository: UserRepository = new InMemoryUserRepository([])
+const userDataArrayWithSingleUser: UserData[] = new Array(validUserSignupRequest)
+const singleUserUserRepository: UserRepository = new InMemoryUserRepository(userDataArrayWithSingleUser)
 const encoder: Encoder = new FakeEncoder()
-const sut: Signup = new Signup(userRepository, encoder)
 
 describe('Signup use case', () => {
   test('should signup user with valid data', async () => {
-    const userSignupResponse: UserData = await sut.perform(userSignupRequest)
-    expect(userSignupResponse).toEqual(userSignupRequest)
-    expect((await userRepository.findAllUsers()).length).toEqual(1)
-    expect((await userRepository.findUserByEmail(validEmail)).password).toEqual(validPassword + 'ENCRYPTED')
+    const sut: Signup = new Signup(emptyUserRepository, encoder)
+    const userSignupResponse = (await sut.perform(validUserSignupRequest))
+    expect(userSignupResponse.value).toEqual(validUserSignupRequest)
+    expect((await emptyUserRepository.findAllUsers()).length).toEqual(1)
+    expect((await emptyUserRepository.findUserByEmail(validEmail)).password).toEqual(validPassword + 'ENCRYPTED')
+  })
+
+  test('should not signup existing user', async () => {
+    const sut: Signup = new Signup(singleUserUserRepository, encoder)
+    const error = await sut.perform(validUserSignupRequest)
+    expect(error.value).toEqual(new ExistingUserError(validUserSignupRequest))
   })
 })
