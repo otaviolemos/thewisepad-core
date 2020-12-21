a7757ed3cf23d448959bfba05fd3e535482431d2
Otavio Lemos <otaviolemos@gmail.com>
2020-12-21 11:15:52 -0300

    refactor: implement correct find algorithm

--- tests/use-cases/in-memory-user-repository.ts
+++ tests/use-cases/in-memory-user-repository.ts
@@ -4,6 +4,10 @@ import { UserData } from '../../src/entities/user-data'
 export class InMemoryUserRepository implements UserRepository {
   private readonly _data: UserData[]
 
+  public get data () {
+    return this._data
+  }
+
   constructor (data: UserData[]) {
     this._data = data
   }
@@ -13,7 +17,8 @@ export class InMemoryUserRepository implements UserRepository {
   }
 
   public async findUserByEmail (email: string): Promise<UserData> {
-    return this._data[0]
+    const found = this.data.find(user => user.email === email)
+    return found || null
   }
 
   public async addUser (userData: UserData): Promise<UserData> {
