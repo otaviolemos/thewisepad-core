0409615f0d2051811f257b964c2b4cb8f5e015f4
Otavio Lemos <otaviolemos@gmail.com>
2020-12-25 14:49:32 -0300

    feat: load notes for registered user

---
+++ src/use-cases/load-notes-for-user/load-notes-for-user.ts
@@ -0,0 +1,14 @@
+import { NoteData } from '../create-note/note-data'
+import { NoteRepository } from '../create-note/ports/note-repository'
+
+export class LoadNotesForUser {
+  private readonly noteRepository: NoteRepository
+
+  constructor (noteRepository: NoteRepository) {
+    this.noteRepository = noteRepository
+  }
+
+  public async perform (requestUserId: string): Promise<NoteData[]> {
+    return await this.noteRepository.findAllNotesFrom(requestUserId)
+  }
+}
---
+++ tests/use-cases/load-notes-for-user/load-notes-for-user.spec.ts
@@ -0,0 +1,33 @@
+import { NoteData } from '../../../src/use-cases/create-note/note-data'
+import { NoteRepository } from '../../../src/use-cases/create-note/ports/note-repository'
+import { LoadNotesForUser } from '../../../src/use-cases/load-notes-for-user/load-notes-for-user'
+import { InMemoryNoteRepository } from '../create-note/in-memory-note-repository'
+
+describe('Load notes for user use case', () => {
+  const validTitle1 = 'my note'
+  const validTitle2 = 'my second note'
+  const validUserId = '0'
+  const someContent = 'some content'
+  const someOtherContent = 'some other content'
+  const note1: NoteData = {
+    title: validTitle1,
+    content: someContent,
+    ownerId: validUserId,
+    id: '0'
+  }
+  const note2: NoteData = {
+    title: validTitle2,
+    content: someOtherContent,
+    ownerId: validUserId,
+    id: '1'
+  }
+  const noteRepositoryWithTwoNotes: NoteRepository = new InMemoryNoteRepository(
+    [note1, note2]
+  )
+
+  test('should correctly load notes for a registered user', async () => {
+    const usecase: LoadNotesForUser = new LoadNotesForUser(noteRepositoryWithTwoNotes)
+    const notes: NoteData[] = await usecase.perform(validUserId)
+    expect(notes.length).toEqual(2)
+  })
+})
