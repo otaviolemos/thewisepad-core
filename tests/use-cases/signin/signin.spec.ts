d0b95cffc0b076afa6756c2639ee363147d3ab05
Otavio Lemos <otaviolemos@gmail.com>
2021-01-02 12:33:49 -0300

    test: return user not found error when trying to sign in with unregistered user

--- tests/use-cases/signin/signin.spec.ts
+++ tests/use-cases/signin/signin.spec.ts
@@ -1,6 +1,7 @@
 import { UserData } from '../../../src/entities/user-data'
 import { UserRepository } from '../../../src/use-cases/ports/user-repository'
 import { WrongPasswordError } from '../../../src/use-cases/signin/errors/wrong-password-error'
+import { UserNotFoundError } from '../../../src/use-cases/signin/errors/user-not-found-error'
 import { Signin } from '../../../src/use-cases/signin/signin'
 import { InMemoryUserRepository } from '../in-memory-user-repository'
 import { FakeEncoder } from '../signup/fake-encoder'
@@ -9,8 +10,10 @@ describe('Signin use case', () => {
   const validEmail = 'any@mail.com'
   const validPassword = '1validpassword'
   const wrongPassword = 'wrongpassword'
+  const unregisteredEmail = 'another@mail.com'
   const validUserSigninRequest: UserData = { email: validEmail, password: validPassword }
-  const invalidUserSigninRequest: UserData = { email: validEmail, password: wrongPassword }
+  const signinRequestWithWrongPassword: UserData = { email: validEmail, password: wrongPassword }
+  const signinRequestWithUnregisteredUser: UserData = { email: unregisteredEmail, password: validPassword }
   const userDataArrayWithSingleUser: UserData[] = new Array({ email: validEmail, password: validPassword + 'ENCRYPTED' })
   const singleUserUserRepository: UserRepository = new InMemoryUserRepository(userDataArrayWithSingleUser)
 
@@ -22,7 +25,13 @@ describe('Signin use case', () => {
 
   test('should not signin if password is incorrect', async () => {
     const usecase = new Signin(singleUserUserRepository, new FakeEncoder())
-    const response = (await (usecase.perform(invalidUserSigninRequest))).value as WrongPasswordError
+    const response = (await (usecase.perform(signinRequestWithWrongPassword))).value as WrongPasswordError
     expect(response.name).toEqual('WrongPasswordError')
+  })
+
+  test('should not signin with unregistered user', async () => {
+    const usecase = new Signin(singleUserUserRepository, new FakeEncoder())
+    const response = (await (usecase.perform(signinRequestWithUnregisteredUser))).value as UserNotFoundError
+    expect(response.name).toEqual('UserNotFoundError')
   })
 })
