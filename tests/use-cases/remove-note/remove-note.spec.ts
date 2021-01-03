4c185a07b4b0022d7d9854083aa658c76bc8de11
Otavio Lemos <otaviolemos@gmail.com>
2021-01-03 14:51:37 -0300

    refactor: use note builder

--- tests/use-cases/remove-note/remove-note.spec.ts
+++ tests/use-cases/remove-note/remove-note.spec.ts
@@ -1,18 +1,15 @@
 import { NoteRepository } from '../../../src/use-cases/ports/note-repository'
 import { RemoveNote } from '../../../src/use-cases/remove-note/remove-note'
+import { NoteBuilder } from '../builders/note-builder'
 import { InMemoryNoteRepository } from '../repositories/in-memory-note-repository'
 
 describe('Remove note use case', () => {
-  const aNote = {
-    title: 'my note',
-    content: 'some content',
-    ownerEmail: 'valid@mail.com',
-    ownerId: '0',
-    id: '0'
-  }
-  const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([aNote])
-
   test('should remove existing note', async () => {
+    const aNote =
+    NoteBuilder
+      .aNote()
+      .build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([aNote])
     const usecase = new RemoveNote(noteRepositoryWithANote)
     const removed = await usecase.perform(aNote.id)
     expect(removed).toBe(aNote)
