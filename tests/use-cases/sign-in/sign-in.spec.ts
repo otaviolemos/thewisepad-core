d866f8dccad3d4c10277eb4b7605bca342a6fbe9
Otavio Lemos <otaviolemos@gmail.com>
2021-01-03 14:56:44 -0300

    refactor: use user builder

--- tests/use-cases/sign-in/sign-in.spec.ts
+++ tests/use-cases/sign-in/sign-in.spec.ts
@@ -1,21 +1,25 @@
 import { UserData } from '../../../src/entities/user-data'
 import { UserRepository } from '../../../src/use-cases/ports/user-repository'
 import { SignIn } from '../../../src/use-cases/sign-in/sign-in'
+import { UserBuilder } from '../builders/user-builder'
 import { InMemoryUserRepository } from '../repositories/in-memory-user-repository'
 import { FakeEncoder } from '../sign-up/fake-encoder'
 
 describe('Sign in use case', () => {
-  const validEmail = 'any@mail.com'
-  const validPassword = '1validpassword'
-  const wrongPassword = 'wrongpassword'
-  const unregisteredEmail = 'another@mail.com'
-  const validUserSigninRequest: UserData = { email: validEmail, password: validPassword }
-  const signInRequestWithWrongPassword: UserData = { email: validEmail, password: wrongPassword }
-  const signInRequestWithUnregisteredUser: UserData = { email: unregisteredEmail, password: validPassword }
-  const userDataArrayWithSingleUser: UserData[] = new Array({ email: validEmail, password: validPassword + 'ENCRYPTED' })
-  const singleUserUserRepository: UserRepository = new InMemoryUserRepository(userDataArrayWithSingleUser)
+  const aUser =
+    UserBuilder
+      .aUser()
+      .build()
+  const userDataArrayWithSingleUser: UserData[] =
+    new Array({ email: aUser.email, password: aUser.password + 'ENCRYPTED' })
+  const singleUserUserRepository: UserRepository =
+    new InMemoryUserRepository(userDataArrayWithSingleUser)
 
   test('should correctly sign in if password is correct', async () => {
+    const validUserSigninRequest: UserData =
+      UserBuilder
+        .aUser()
+        .build()
     const usecase = new SignIn(singleUserUserRepository, new FakeEncoder())
     const userResponse = (await (usecase.perform(validUserSigninRequest))).value
     expect(userResponse).toEqual(validUserSigninRequest)
@@ -23,12 +27,22 @@ describe('Sign in use case', () => {
 
   test('should not sign in if password is incorrect', async () => {
     const usecase = new SignIn(singleUserUserRepository, new FakeEncoder())
+    const signInRequestWithWrongPassword: UserData =
+      UserBuilder
+        .aUser()
+        .build()
+    signInRequestWithWrongPassword.password = 'wrongpassword'
     const response = (await (usecase.perform(signInRequestWithWrongPassword))).value as Error
     expect(response.name).toEqual('WrongPasswordError')
   })
 
   test('should not sign in with unregistered user', async () => {
     const usecase = new SignIn(singleUserUserRepository, new FakeEncoder())
+    const signInRequestWithUnregisteredUser: UserData =
+      UserBuilder
+        .aUser()
+        .build()
+    signInRequestWithUnregisteredUser.email = 'unregistered@mail.com'
     const response = (await (usecase.perform(signInRequestWithUnregisteredUser))).value as Error
     expect(response.name).toEqual('UserNotFoundError')
   })
