41eaf33eb4ca11711867055c0e92f2b6adb73243
Otavio Lemos <otaviolemos@gmail.com>
2021-01-16 16:59:32 -0300

    feat: handle internal server errors

--- src/controllers/sign-up.ts
+++ src/controllers/sign-up.ts
@@ -1,7 +1,7 @@
 import { UserData } from '@/use-cases/ports'
 import { SignUp } from '@/use-cases/sign-up'
 import { HttpResponse } from '@/controllers/ports'
-import { badRequest, created, forbidden } from '@/controllers/util'
+import { badRequest, created, forbidden, serverError } from '@/controllers/util'
 import { ExistingUserError } from '@/use-cases/sign-up/errors'
 
 export class SignUpController {
@@ -12,17 +12,21 @@ export class SignUpController {
   }
 
   async handle (request: UserData): Promise<HttpResponse> {
-    const response = await this.usecase.perform(request)
+    try {
+      const response = await this.usecase.perform(request)
 
-    if (response.isRight()) {
-      return created(response.value)
-    }
-
-    if (response.isLeft()) {
-      if (response.value instanceof ExistingUserError) {
-        return forbidden(response.value)
+      if (response.isRight()) {
+        return created(response.value)
       }
-      return badRequest(response.value)
+
+      if (response.isLeft()) {
+        if (response.value instanceof ExistingUserError) {
+          return forbidden(response.value)
+        }
+        return badRequest(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
--- src/controllers/util/http-helper.ts
+++ src/controllers/util/http-helper.ts
@@ -19,3 +19,8 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
+})
