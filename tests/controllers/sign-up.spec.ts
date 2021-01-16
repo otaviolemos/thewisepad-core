cea1d7c77feb76e3446328a54e5810310dc78b77
Otavio Lemos <otaviolemos@gmail.com>
2021-01-16 13:57:00 -0300

    test: handle existing user request

--- tests/controllers/sign-up.spec.ts
+++ tests/controllers/sign-up.spec.ts
@@ -7,14 +7,23 @@ import { InMemoryUserRepository } from '@test/use-cases/repositories'
 import { FakeEncoder } from '@test/use-cases/sign-up'
 
 describe('Sign up controller', () => {
+  const emptyUserRepository = new InMemoryUserRepository([])
+  const encoder: Encoder = new FakeEncoder()
+  const signUpUseCase: SignUp = new SignUp(emptyUserRepository, encoder)
+  const controller = new SignUpController(signUpUseCase)
+  const validUserSignUpRequest = UserBuilder.aUser().build()
+
   test('should return 200 and registered user when user is successfully signed up', async () => {
-    const emptyUserRepository = new InMemoryUserRepository([])
-    const encoder: Encoder = new FakeEncoder()
-    const usecase: SignUp = new SignUp(emptyUserRepository, encoder)
-    const validUserSignUpRequest = UserBuilder.aUser().build()
-    const controller = new SignUpController(usecase)
+    validUserSignUpRequest.id = undefined
     const response: HttpResponse = await controller.handle(validUserSignUpRequest)
     expect(response.statusCode).toEqual(200)
     expect((response.body as UserData).id).toEqual('0')
+  })
+
+  test('should return 403 when trying to sign up existing user', async () => {
+    await controller.handle(validUserSignUpRequest)
+    const response: HttpResponse = await controller.handle(validUserSignUpRequest)
+    expect(response.statusCode).toEqual(403)
+    expect((response.body as Error).name).toEqual('ExistingUserError')
   })
 })
