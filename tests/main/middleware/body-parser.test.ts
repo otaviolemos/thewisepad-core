fad2c316e08d59db0c921f96d7f6ad104372b1c1
Otavio Lemos <otaviolemos@gmail.com>
2021-01-29 12:38:53 -0300

    test: enable json body parser middleware

---
+++ tests/main/middleware/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Body parser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Otavio' })
+      .expect({ name: 'Otavio' })
+  })
+})
