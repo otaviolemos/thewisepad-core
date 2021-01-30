a860e237686ce1f4b80c9592be111210aa7aec57
Otavio Lemos <otaviolemos@gmail.com>
2021-01-30 14:03:35 -0300

    test: setup sign up route

---
+++ tests/main/routes/sign-up-route.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Register Routes', () => {
+  test('should return an account on success', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/api/signup')
+      .send({
+        email: 'any@mail.com',
+        password: '1validpassword'
+      })
+      .expect(201, {
+        email: 'any@mail.com',
+        password: '1validpasswordENCRYPTED',
+        id: '0'
+      })
+  })
+})
