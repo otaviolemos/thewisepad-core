8b1d4e9a1cb63cb9d1feae75079fff4189d80a73
Otavio Lemos <otaviolemos@gmail.com>
2021-02-06 15:38:17 -0300

    refactor: make rounds a private configurable field

--- src/external/encoder/bcrypt-encoder.ts
+++ src/external/encoder/bcrypt-encoder.ts
@@ -2,10 +2,14 @@ import { Encoder } from '@/use-cases/ports'
 import * as bcrypt from 'bcrypt'
 
 export class BcryptEncoder implements Encoder {
-  private static readonly rounds: number = 10
+  private readonly rounds: number = 10
+
+  constructor (rounds: number) {
+    this.rounds = rounds
+  }
 
   async encode (plain: string): Promise<string> {
-    return await bcrypt.hash(plain, BcryptEncoder.rounds)
+    return await bcrypt.hash(plain, this.rounds)
   }
 
   async compare (plain: string, hashed: string): Promise<boolean> {
