895aa5e222907e84a8fc68a073a225ee59d77681
Otavio Lemos <otaviolemos@gmail.com>
2021-02-06 16:12:03 -0300

    feat: jwt token manager

---
+++ src/external/token-manager/jwt-token-manager.ts
@@ -0,0 +1,24 @@
+import { Either, left, right } from '@/shared'
+import { Payload, TokenManager } from '@/use-cases/authentication/ports'
+import * as jwt from 'jsonwebtoken'
+
+export class JwtTokenManager implements TokenManager {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async sign (info: Payload): Promise<string> {
+    return jwt.sign(info, this.secret)
+  }
+
+  async verify (token: string): Promise<Either<Error, string | object>> {
+    try {
+      const decoded = jwt.verify(token, this.secret)
+      return right(decoded)
+    } catch (error) {
+      return left(error)
+    }
+  }
+}
