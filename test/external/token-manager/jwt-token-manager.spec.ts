b0c8576a73cc58769e1778780885d82ddec2d48e
Otavio Lemos <otaviolemos@gmail.com>
2021-02-07 12:10:10 -0300

    test: verify expired json web token

--- test/external/token-manager/jwt-token-manager.spec.ts
+++ test/external/token-manager/jwt-token-manager.spec.ts
@@ -1,5 +1,6 @@
 import { JwtTokenManager } from '@/external/token-manager'
 import { Payload } from '@/use-cases/authentication/ports'
+import * as sinon from 'sinon'
 
 describe('JWT token manager', () => {
   test('should correctly sign and verify a json web token', async () => {
@@ -23,12 +24,15 @@ describe('JWT token manager', () => {
   })
 
   test('should correctly verify expired json web tokens', async () => {
+    const clock = sinon.useFakeTimers()
     const secret = 'my secret'
     const tokenManager = new JwtTokenManager(secret)
     const info: Payload = { id: 'my id' }
-    const exp = '0.1s'
+    const exp = '1h'
     const signedToken = await tokenManager.sign(info, exp)
+    clock.tick(3600100)
     expect(signedToken).not.toEqual(info)
-    // todo: expect((await tokenManager.verify(signedToken) as Error).name).toEqual('TokenExpiredError')
+    expect(((await (tokenManager.verify(signedToken))).value as Error).name).toEqual('TokenExpiredError')
+    clock.restore()
   })
 })
