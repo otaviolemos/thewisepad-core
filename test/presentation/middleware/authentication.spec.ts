d6ec2f3a297fc6c7f168d175a2e75c0393af0ba3
Otavio Lemos <otaviolemos@gmail.com>
2021-02-13 14:13:39 -0300

    test: simplify code

--- test/presentation/middleware/authentication.spec.ts
+++ test/presentation/middleware/authentication.spec.ts
@@ -2,24 +2,23 @@ import { HttpResponse } from '@/presentation/controllers/ports'
 import { Authentication } from '@/presentation/middleware'
 import { FakeTokenManager } from '@test/doubles/authentication'
 import { forbidden, ok, serverError } from '@/presentation/controllers/util/http-helper'
-import { MissingParamError } from '@/presentation/controllers/errors'
 import { Payload, TokenManager } from '@/use-cases/authentication/ports'
 import { Either } from '@/shared'
 
 describe('Authentication middleware', () => {
-  test('should return forbidden if access token is empty', async () => {
+  test('should return forbidden with missing param error if access token is empty', async () => {
     const authMiddleware = new Authentication(new FakeTokenManager())
     const response: HttpResponse = await authMiddleware.handle({ accessToken: '' })
-    expect(response).toEqual(forbidden(new MissingParamError('accessToken')))
+    expect(response).toEqual(forbidden(new Error('Invalid token.')))
   })
 
-  test('should return forbidden if access token is null', async () => {
+  test('should return forbidden with missing param error if access token is null', async () => {
     const authMiddleware = new Authentication(new FakeTokenManager())
     const response: HttpResponse = await authMiddleware.handle({ accessToken: null })
-    expect(response).toEqual(forbidden(new MissingParamError('accessToken')))
+    expect(response).toEqual(forbidden(new Error('Invalid token.')))
   })
 
-  test('should return forbidden if access token is invalid', async () => {
+  test('should return forbidden with invalid token error if access token is invalid', async () => {
     const tokenManager = new FakeTokenManager()
     const payload = { id: 'my id' }
     const token = await tokenManager.sign(payload)
