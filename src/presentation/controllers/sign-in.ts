cc7e7253a7037d0f5e86a5763a59bb82bfcef93b
Otavio Lemos <otaviolemos@gmail.com>
2021-02-19 14:33:48 -0300

    feat: verify if request contains email and password

--- src/presentation/controllers/sign-in.ts
+++ src/presentation/controllers/sign-in.ts
@@ -1,6 +1,7 @@
 import { UseCase } from '@/use-cases/ports'
 import { HttpRequest, HttpResponse, WebController } from '@/presentation/controllers/ports'
-import { ok } from '@/presentation/controllers/util'
+import { badRequest, ok } from '@/presentation/controllers/util'
+import { MissingParamError } from '@/presentation/controllers/errors'
 
 export class SignInController implements WebController {
   protected readonly signInUseCase: UseCase
@@ -10,6 +11,12 @@ export class SignInController implements WebController {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
+    if (!(request.body.email) || !(request.body.password)) {
+      let missingParam = !(request.body.email) ? 'email ' : ''
+      missingParam += !(request.body.password) ? 'password' : ''
+      return badRequest(new MissingParamError(missingParam.trim()))
+    }
+
     const response = await this.signInUseCase.perform({ email: request.body.email, password: request.body.password })
 
     if (response.isRight()) {
