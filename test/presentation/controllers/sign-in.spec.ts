7477449a96f5b9866d2aab42cc5cbcb7440af747
Otavio Lemos <otaviolemos@gmail.com>
2021-02-19 14:38:24 -0300

    test: verify if request contains password and email

--- test/presentation/controllers/sign-in.spec.ts
+++ test/presentation/controllers/sign-in.spec.ts
@@ -61,6 +61,20 @@ describe('Sign in controller', () => {
     expect((response.body as Error).message).toEqual('Missing parameter: password.')
   })
 
+  test('should return 400 if password and email are missing in the request', async () => {
+    const singleUserUserRepository = await getSingleUserUserRepository()
+    const usecase = new SignIn(new CustomAuthentication(singleUserUserRepository, new FakeEncoder(), new FakeTokenManager()))
+    const siginRequestWithoutEmail: HttpRequest = {
+      body: {
+      }
+    }
+    const controller = new SignInController(usecase)
+    const response: HttpResponse = await controller.handle(siginRequestWithoutEmail)
+    expect(response.statusCode).toEqual(400)
+    expect(response.body).toBeInstanceOf(MissingParamError)
+    expect((response.body as Error).message).toEqual('Missing parameter: email password.')
+  })
+
   async function getSingleUserUserRepository (): Promise<UserRepository> {
     const aUser = UserBuilder.aUser().build()
     const userDataArrayWithSingleUser: UserData[] =
