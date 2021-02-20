2647776670f6bf8eb9d44f591435e64af61349b8
Otavio Lemos <otaviolemos@gmail.com>
2021-02-20 18:38:53 -0300

    feat: return 500 when server throws

--- src/presentation/controllers/create-note.ts
+++ src/presentation/controllers/create-note.ts
@@ -3,7 +3,7 @@ import { HttpRequest, HttpResponse, WebController } from '@/presentation/control
 import { Either } from '@/shared'
 import { ExistingTitleError, UnregisteredOwnerError } from '@/use-cases/create-note/errors'
 import { NoteData, UseCase } from '@/use-cases/ports'
-import { created, badRequest, getMissingParams } from '@/presentation/controllers/util'
+import { created, badRequest, getMissingParams, serverError } from '@/presentation/controllers/util'
 import { MissingParamError } from '@/presentation/controllers/errors'
 
 export class CreateNoteController implements WebController {
@@ -14,22 +14,26 @@ export class CreateNoteController implements WebController {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const missingParams: string[] = getMissingParams(request, ['title', 'content', 'ownerEmail'])
-    if (missingParams.length > 0) {
-      return badRequest(new MissingParamError(missingParams.join(', ')))
-    }
-    const noteRequest: NoteData = {
-      title: request.body.title,
-      content: request.body.content,
-      ownerEmail: request.body.ownerEmail
-    }
-    const useCaseResponse: Either<ExistingTitleError | UnregisteredOwnerError | InvalidTitleError, NoteData> =
-      await this.useCase.perform(noteRequest)
+    try {
+      const missingParams: string[] = getMissingParams(request, ['title', 'content', 'ownerEmail'])
+      if (missingParams.length > 0) {
+        return badRequest(new MissingParamError(missingParams.join(', ')))
+      }
+      const noteRequest: NoteData = {
+        title: request.body.title,
+        content: request.body.content,
+        ownerEmail: request.body.ownerEmail
+      }
+      const useCaseResponse: Either<ExistingTitleError | UnregisteredOwnerError | InvalidTitleError, NoteData> =
+        await this.useCase.perform(noteRequest)
 
-    if (useCaseResponse.isRight()) {
-      return created(useCaseResponse.value)
-    }
+      if (useCaseResponse.isRight()) {
+        return created(useCaseResponse.value)
+      }
 
-    return badRequest(useCaseResponse.value)
+      return badRequest(useCaseResponse.value)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
