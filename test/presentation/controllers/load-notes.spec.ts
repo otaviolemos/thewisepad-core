e4345e45abb6cb854fb46532e46772ebab1c57ce
Otavio Lemos <otaviolemos@gmail.com>
2021-03-03 16:49:33 -0300

    test: return 200 and notes for user

---
+++ test/presentation/controllers/load-notes.spec.ts
@@ -0,0 +1,28 @@
+import { LoadNotesController } from '@/presentation/controllers/load-notes'
+import { HttpRequest, HttpResponse } from '@/presentation/controllers/ports'
+import { LoadNotes } from '@/use-cases/load-notes'
+import { NoteData, NoteRepository } from '@/use-cases/ports'
+import { NoteBuilder, UserBuilder } from '@test/builders'
+import { InMemoryNoteRepository } from '@test/doubles/repositories'
+
+describe('Load notes controller', () => {
+  test('should return 200 when load notes use case returns', async () => {
+    const note1: NoteData = NoteBuilder.aNote().build()
+    const note2: NoteData = NoteBuilder.aNote().withDifferentTitleAndId().build()
+    const noteRepositoryWithTwoNotes: NoteRepository = new InMemoryNoteRepository(
+      [note1, note2]
+    )
+    const usecase: LoadNotes = new LoadNotes(noteRepositoryWithTwoNotes)
+    const loadNotesController: LoadNotesController = new LoadNotesController(usecase)
+    const aUser = UserBuilder.aUser().build()
+    const loadNotesRequest: HttpRequest = {
+      body: {
+        userId: aUser.id
+      }
+    }
+    const response: HttpResponse = await loadNotesController.handle(loadNotesRequest)
+    const loadResult = response.body as NoteData[]
+    expect(loadResult.length).toEqual(2)
+    expect(response.statusCode).toEqual(200)
+  })
+})
