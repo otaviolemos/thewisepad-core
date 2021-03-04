02a41e67ec22e17732cebbd1fb0d7eb9e067d944
Otavio Lemos <otaviolemos@gmail.com>
2021-03-04 16:52:10 -0300

    test: additional cases

--- test/use-cases/update-note/update-note.spec.ts
+++ test/use-cases/update-note/update-note.spec.ts
@@ -1,22 +1,103 @@
 import { NoteData, NoteRepository, UserRepository } from '@/use-cases/ports'
-import { UpdateNote } from '@/use-cases/update-note'
+import { UpdateNoteRequest, UpdateNote } from '@/use-cases/update-note'
 import { InMemoryNoteRepository, InMemoryUserRepository } from '@test/doubles/repositories'
 import { UserBuilder, NoteBuilder } from '@test/builders'
+import { ExistingTitleError } from '@/use-cases/create-note/errors'
+import { InvalidTitleError } from '@/entities/errors'
 
 describe('Update note use case', () => {
   test('should update title and content of existing note', async () => {
     const originalNote: NoteData = NoteBuilder.aNote().build()
-    const changedNote: NoteData =
-      NoteBuilder.aNote().withDifferentTitleAndContent().build()
+    const noteWithDifferentTitleAndContent: NoteData = NoteBuilder.aNote().withDifferentTitleAndContent().build()
+    const changedNote: UpdateNoteRequest = {
+      title: noteWithDifferentTitleAndContent.title,
+      content: noteWithDifferentTitleAndContent.content,
+      id: noteWithDifferentTitleAndContent.id,
+      ownerEmail: noteWithDifferentTitleAndContent.ownerEmail,
+      ownerId: noteWithDifferentTitleAndContent.ownerId
+    }
     const owner = UserBuilder.aUser().build()
     const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([originalNote])
     const userRepositoryWithAUser: UserRepository = new InMemoryUserRepository([
       owner
     ])
     const usecase = new UpdateNote(noteRepositoryWithANote, userRepositoryWithAUser)
-    changedNote.id = originalNote.id
     const response = (await usecase.perform(changedNote)).value as NoteData
     expect(response.title).toEqual(changedNote.title)
     expect(response.content).toEqual(changedNote.content)
+  })
+
+  test('should not update title of existing note if user already has note with same title', async () => {
+    const originalNote: NoteData = NoteBuilder.aNote().build()
+    const aSecondNote: NoteData = NoteBuilder.aNote().withDifferentTitleAndContent().build()
+    const changedNote: UpdateNoteRequest = {
+      title: aSecondNote.title,
+      ownerEmail: originalNote.ownerEmail,
+      content: originalNote.content,
+      id: originalNote.id,
+      ownerId: originalNote.ownerId
+    }
+    const owner = UserBuilder.aUser().build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([originalNote, aSecondNote])
+    const userRepositoryWithAUser: UserRepository = new InMemoryUserRepository([
+      owner
+    ])
+    const usecase = new UpdateNote(noteRepositoryWithANote, userRepositoryWithAUser)
+    const response = (await usecase.perform(changedNote)).value as Error
+    expect(response).toBeInstanceOf(ExistingTitleError)
+  })
+
+  test('should update title of existing note', async () => {
+    const originalNote: NoteData = NoteBuilder.aNote().build()
+    const changedNote = {
+      title: 'A different title',
+      ownerEmail: originalNote.ownerEmail,
+      id: originalNote.id,
+      ownerId: originalNote.ownerId
+    }
+    const owner = UserBuilder.aUser().build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([originalNote])
+    const userRepositoryWithAUser: UserRepository = new InMemoryUserRepository([
+      owner
+    ])
+    const usecase = new UpdateNote(noteRepositoryWithANote, userRepositoryWithAUser)
+    const response = (await usecase.perform(changedNote)).value as NoteData
+    expect(response.title).toEqual(changedNote.title)
+  })
+
+  test('should update content of existing note', async () => {
+    const originalNote: NoteData = NoteBuilder.aNote().build()
+    const changedNote = {
+      ownerEmail: originalNote.ownerEmail,
+      id: originalNote.id,
+      ownerId: originalNote.ownerId,
+      content: 'A different content'
+    }
+    const owner = UserBuilder.aUser().build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([originalNote])
+    const userRepositoryWithAUser: UserRepository = new InMemoryUserRepository([
+      owner
+    ])
+    const usecase = new UpdateNote(noteRepositoryWithANote, userRepositoryWithAUser)
+    const response = (await usecase.perform(changedNote)).value as NoteData
+    expect(response.content).toEqual(changedNote.content)
+  })
+
+  test('should not update title with invalid title', async () => {
+    const originalNote: NoteData = NoteBuilder.aNote().build()
+    const changedNote = {
+      title: ' ',
+      ownerEmail: originalNote.ownerEmail,
+      id: originalNote.id,
+      ownerId: originalNote.ownerId
+    }
+    const owner = UserBuilder.aUser().build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([originalNote])
+    const userRepositoryWithAUser: UserRepository = new InMemoryUserRepository([
+      owner
+    ])
+    const usecase = new UpdateNote(noteRepositoryWithANote, userRepositoryWithAUser)
+    const response = (await usecase.perform(changedNote)).value as Error
+    expect(response).toBeInstanceOf(InvalidTitleError)
   })
 })
