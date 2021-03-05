48d6a98ea36693ffc769841e695660282cf1eda5
Otavio Lemos <otaviolemos@gmail.com>
2021-03-05 16:59:06 -0300

    test: handle invalid title note update

--- test/presentation/controllers/update-note.spec.ts
+++ test/presentation/controllers/update-note.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidTitleError } from '@/entities/errors'
 import { HttpRequest, HttpResponse } from '@/presentation/controllers/ports'
 import { UpdateNoteController } from '@/presentation/controllers/update-note'
 import { NoteData, NoteRepository, UserRepository } from '@/use-cases/ports'
@@ -30,5 +31,28 @@ describe('Update note controller', () => {
     expect(response.statusCode).toEqual(200)
     expect((response.body as NoteData).content).toEqual(changedNote.content)
     expect((response.body as NoteData).title).toEqual(changedNote.title)
+  })
+
+  test('should return 400 when trying to update note with invalid title', async () => {
+    const originalNote: NoteData = NoteBuilder.aNote().build()
+    const changedNote: UpdateNoteRequest = {
+      title: '',
+      id: originalNote.id,
+      ownerEmail: originalNote.ownerEmail,
+      ownerId: originalNote.ownerId
+    }
+    const request: HttpRequest = {
+      body: changedNote
+    }
+    const owner = UserBuilder.aUser().build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([originalNote])
+    const userRepositoryWithAUser: UserRepository = new InMemoryUserRepository([
+      owner
+    ])
+    const usecase = new UpdateNote(noteRepositoryWithANote, userRepositoryWithAUser)
+    const controller = new UpdateNoteController(usecase)
+    const response: HttpResponse = await controller.handle(request)
+    expect(response.statusCode).toEqual(400)
+    expect(response.body).toBeInstanceOf(InvalidTitleError)
   })
 })
