ccfef7c4af61d67d5a0f529a039264ee09d354ac
Otavio Lemos <otaviolemos@gmail.com>
2021-03-05 16:49:46 -0300

    feat: handle successful note update

---
+++ src/presentation/controllers/update-note.ts
@@ -0,0 +1,22 @@
+import { InvalidTitleError } from '@/entities/errors'
+import { HttpRequest, HttpResponse, WebController } from '@/presentation/controllers/ports'
+import { Either } from '@/shared'
+import { ExistingTitleError } from '@/use-cases/create-note/errors'
+import { NoteData, UseCase } from '@/use-cases/ports'
+
+export class UpdateNoteController implements WebController {
+  private readonly updateNoteUseCase: UseCase
+  constructor (updateNoteUseCase: UseCase) {
+    this.updateNoteUseCase = updateNoteUseCase
+  }
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    const useCaseResponse: Either<ExistingTitleError | InvalidTitleError, NoteData> =
+     await this.updateNoteUseCase.perform(request.body)
+    const response: HttpResponse = {
+      statusCode: 200,
+      body: useCaseResponse.value
+    }
+    return response
+  }
+}
