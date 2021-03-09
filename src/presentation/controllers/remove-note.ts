e1a7376bdfeab1480d1058fc02b21d76f75f3573
Otavio Lemos <otaviolemos@gmail.com>
2021-03-09 15:33:23 -0300

    feat: handle successful note removal

---
+++ src/presentation/controllers/remove-note.ts
@@ -0,0 +1,22 @@
+import { HttpRequest, HttpResponse, WebController } from '@/presentation/controllers/ports'
+import { Either } from '@/shared'
+import { NoteData, UseCase } from '@/use-cases/ports'
+import { UnexistingNoteError } from '@/use-cases/remove-note/errors'
+import { ok } from '@/presentation/controllers/util'
+
+export class RemoveNoteController implements WebController {
+  private readonly removeNoteUseCase: UseCase
+
+  constructor (removeNoteUseCase: UseCase) {
+    this.removeNoteUseCase = removeNoteUseCase
+  }
+
+  async handle (request: HttpRequest): Promise<HttpResponse> {
+    const useCaseResponse: Either<UnexistingNoteError, NoteData> =
+      await this.removeNoteUseCase.perform(request.body.noteId)
+
+    if (useCaseResponse.isRight()) {
+      return ok(useCaseResponse.value)
+    }
+  }
+}
