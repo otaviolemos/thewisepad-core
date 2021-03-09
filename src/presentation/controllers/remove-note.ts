7dad8656fa87d390077660e36e11d51c88a7d6d3
Otavio Lemos <otaviolemos@gmail.com>
2021-03-09 15:43:58 -0300

    feat: handle server error

--- src/presentation/controllers/remove-note.ts
+++ src/presentation/controllers/remove-note.ts
@@ -2,7 +2,7 @@ import { HttpRequest, HttpResponse, WebController } from '@/presentation/control
 import { Either } from '@/shared'
 import { NoteData, UseCase } from '@/use-cases/ports'
 import { UnexistingNoteError } from '@/use-cases/remove-note/errors'
-import { badRequest, getMissingParams, ok } from '@/presentation/controllers/util'
+import { badRequest, getMissingParams, ok, serverError } from '@/presentation/controllers/util'
 import { MissingParamError } from '@/presentation/controllers/errors'
 
 export class RemoveNoteController implements WebController {
@@ -13,18 +13,22 @@ export class RemoveNoteController implements WebController {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const requiredParams = ['noteId']
-    const missingParams: string = getMissingParams(request, requiredParams)
-    if (missingParams) {
-      return badRequest(new MissingParamError(missingParams))
-    }
-    const useCaseResponse: Either<UnexistingNoteError, NoteData> =
-      await this.removeNoteUseCase.perform(request.body.noteId)
+    try {
+      const requiredParams = ['noteId']
+      const missingParams: string = getMissingParams(request, requiredParams)
+      if (missingParams) {
+        return badRequest(new MissingParamError(missingParams))
+      }
+      const useCaseResponse: Either<UnexistingNoteError, NoteData> =
+        await this.removeNoteUseCase.perform(request.body.noteId)
 
-    if (useCaseResponse.isRight()) {
-      return ok(useCaseResponse.value)
-    }
+      if (useCaseResponse.isRight()) {
+        return ok(useCaseResponse.value)
+      }
 
-    return badRequest(useCaseResponse.value)
+      return badRequest(useCaseResponse.value)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
