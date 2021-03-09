c307952658b1f485e0df71cb32bb7d6f66de5c28
Otavio Lemos <otaviolemos@gmail.com>
2021-03-09 15:35:54 -0300

    feat: handle attemp to remove unexisting note

--- src/presentation/controllers/remove-note.ts
+++ src/presentation/controllers/remove-note.ts
@@ -2,7 +2,7 @@ import { HttpRequest, HttpResponse, WebController } from '@/presentation/control
 import { Either } from '@/shared'
 import { NoteData, UseCase } from '@/use-cases/ports'
 import { UnexistingNoteError } from '@/use-cases/remove-note/errors'
-import { ok } from '@/presentation/controllers/util'
+import { badRequest, ok } from '@/presentation/controllers/util'
 
 export class RemoveNoteController implements WebController {
   private readonly removeNoteUseCase: UseCase
@@ -18,5 +18,7 @@ export class RemoveNoteController implements WebController {
     if (useCaseResponse.isRight()) {
       return ok(useCaseResponse.value)
     }
+
+    return badRequest(useCaseResponse.value)
   }
 }
