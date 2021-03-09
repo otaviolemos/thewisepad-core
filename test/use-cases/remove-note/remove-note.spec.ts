8fa579e8574af117871fbcc478739f94cb87502a
Otavio Lemos <otaviolemos@gmail.com>
2021-03-09 15:17:49 -0300

    test: handle attempt to remove unexisting note

--- test/use-cases/remove-note/remove-note.spec.ts
+++ test/use-cases/remove-note/remove-note.spec.ts
@@ -1,5 +1,6 @@
 import { NoteRepository } from '@/use-cases/ports'
 import { RemoveNote } from '@/use-cases/remove-note'
+import { UnexistingNoteError } from '@/use-cases/remove-note/errors'
 import { NoteBuilder } from '@test/builders'
 import { InMemoryNoteRepository } from '@test/doubles/repositories'
 
@@ -11,8 +12,24 @@ describe('Remove note use case', () => {
       .build()
     const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([aNote])
     const usecase = new RemoveNote(noteRepositoryWithANote)
-    const removed = await usecase.perform(aNote.id)
+    const removed = (await usecase.perform(aNote.id)).value
     expect(removed).toBe(aNote)
     expect(await noteRepositoryWithANote.findById(aNote.id)).toBeNull()
+  })
+
+  test('should return error if removing unexisting note', async () => {
+    const aNote =
+    NoteBuilder
+      .aNote()
+      .build()
+    const anotherNote =
+      NoteBuilder
+        .aNote()
+        .withDifferentTitleAndId()
+        .build()
+    const noteRepositoryWithANote: NoteRepository = new InMemoryNoteRepository([aNote])
+    const usecase = new RemoveNote(noteRepositoryWithANote)
+    const error = (await usecase.perform(anotherNote.id)).value as Error
+    expect(error).toBeInstanceOf(UnexistingNoteError)
   })
 })
