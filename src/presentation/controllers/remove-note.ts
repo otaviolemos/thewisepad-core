45f7bb76bdcf75eddf13b0ac22da8beb8fbe6d93
Otavio Lemos <otaviolemos@gmail.com>
2021-03-09 15:40:50 -0300

    feat: handle missing required parameter

--- src/presentation/controllers/remove-note.ts
+++ src/presentation/controllers/remove-note.ts
@@ -2,7 +2,8 @@ import { HttpRequest, HttpResponse, WebController } from '@/presentation/control
 import { Either } from '@/shared'
 import { NoteData, UseCase } from '@/use-cases/ports'
 import { UnexistingNoteError } from '@/use-cases/remove-note/errors'
-import { badRequest, ok } from '@/presentation/controllers/util'
+import { badRequest, getMissingParams, ok } from '@/presentation/controllers/util'
+import { MissingParamError } from '@/presentation/controllers/errors'
 
 export class RemoveNoteController implements WebController {
   private readonly removeNoteUseCase: UseCase
@@ -12,6 +13,11 @@ export class RemoveNoteController implements WebController {
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
+    const requiredParams = ['noteId']
+    const missingParams: string = getMissingParams(request, requiredParams)
+    if (missingParams) {
+      return badRequest(new MissingParamError(missingParams))
+    }
     const useCaseResponse: Either<UnexistingNoteError, NoteData> =
       await this.removeNoteUseCase.perform(request.body.noteId)
 
