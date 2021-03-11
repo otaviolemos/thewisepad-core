3deb4913c6ead3e9fe45a1b5baccba89ba6ff3dc
Otavio Lemos <otaviolemos@gmail.com>
2021-03-11 16:25:58 -0300

    feat: handle missing param

--- src/presentation/controllers/load-notes.ts
+++ src/presentation/controllers/load-notes.ts
@@ -1,5 +1,7 @@
 import { HttpRequest, HttpResponse, WebController } from '@/presentation/controllers/ports'
 import { UseCase } from '@/use-cases/ports'
+import { badRequest, getMissingParams } from '@/presentation/controllers/util'
+import { MissingParamError } from '@/presentation/controllers/errors'
 
 export class LoadNotesController implements WebController {
   private readonly loadNotesUseCase: UseCase
@@ -9,6 +11,11 @@ export class LoadNotesController implements WebController {
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredParam = ['userId']
+      const missingParams: string = getMissingParams(request, requiredParam)
+      if (missingParams) {
+        return badRequest(new MissingParamError(missingParams))
+      }
       return {
         statusCode: 200,
         body: await this.loadNotesUseCase.perform(request.body.userId)
