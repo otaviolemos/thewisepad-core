d3fb80074289de10b2bbec43dde5f92054522909
Otavio Lemos <otaviolemos@gmail.com>
2021-03-13 12:50:50 -0300

    test: add and find valid note by id

---
+++ test/external/repositories/mongodb/mongodb-note-repository.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helpers'
+import { MongodbNoteRepository } from '@/external/repositories/mongodb/mongodb-note-repository'
+import { NoteData } from '@/use-cases/ports'
+import { NoteBuilder } from '@test/builders'
+
+describe('Mongodb User repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await MongoHelper.clearCollection('notes')
+  })
+
+  test('should add valid note', async () => {
+    const repository = new MongodbNoteRepository()
+    const aValidNote = NoteBuilder.aNote().build()
+    const addedNote: NoteData = await repository.add(aValidNote)
+    const foundNote = await repository.findById(addedNote.id)
+    expect(foundNote).toBeDefined()
+  })
+})
