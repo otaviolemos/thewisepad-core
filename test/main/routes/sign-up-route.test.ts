52f639ae41ec9031806567224f3f3905c5643f87
Otavio Lemos <otaviolemos@gmail.com>
2021-03-14 19:41:20 -0300

    feat: connect to mongodb and check payload

--- test/main/routes/sign-up-route.test.ts
+++ test/main/routes/sign-up-route.test.ts
@@ -1,7 +1,16 @@
 import request from 'supertest'
 import app from '@/main/config/app'
+import { MongoHelper } from '@/external/repositories/mongodb/helpers'
 
-describe('Register Routes', () => {
+describe('Signup route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should return an account on success', async () => {
     app.post('/test_cors', (req, res) => {
       res.send()
@@ -12,9 +21,10 @@ describe('Register Routes', () => {
         email: 'any@mail.com',
         password: '1validpassword'
       })
-      .expect(201, {
-        accessToken: 'accessToken',
-        id: '0'
+      .expect(201)
+      .then((res) => {
+        expect(res.body.accessToken).toBeDefined()
+        expect(res.body.id).toBeDefined()
       })
   })
 })
