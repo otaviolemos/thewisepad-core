ae94bbf078f9d0851947b364ece5df4e142085f0
Otavio Lemos <otaviolemos@gmail.com>
2021-03-18 16:13:36 -0300

    fix: remove '?'

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from 'express'
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const request = {
-      accessToken: req.headers?.['x-access-token'],
+      accessToken: req.headers['x-access-token'],
       ...(req.headers || {})
     }
     const httpResponse = await middleware.handle(request)
