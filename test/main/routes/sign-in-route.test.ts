c443dd63f25056324214de525a8dd471dc3f2665
Otavio Lemos <otaviolemos@gmail.com>
2021-03-18 14:23:15 -0300

    refactor: make test independent of sign up

--- test/main/routes/sign-in-route.test.ts
+++ test/main/routes/sign-in-route.test.ts
@@ -1,34 +1,36 @@
 import request from 'supertest'
 import app from '@/main/config/app'
 import { MongoHelper } from '@/external/repositories/mongodb/helpers'
+import { MongodbUserRepository } from '@/external/repositories/mongodb/mondodb-user-repository'
+import { UserBuilder } from '@test/builders'
+import { BcryptEncoder } from '@/external/encoder'
 
 describe('Signin route', () => {
+  const validUser = UserBuilder.aUser().build()
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
+    const repo = new MongodbUserRepository()
+    const encoder = new BcryptEncoder(parseInt(process.env.BCRYPT_ROUNDS))
+    await repo.add({
+      email: validUser.email,
+      password: await encoder.encode(validUser.password)
+    })
   })
 
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
 
-  test('should successfully sign in', async () => {
-    app.post('/test_cors', (req, res) => {
-      res.send()
-    })
-    await request(app)
-      .post('/api/signup')
-      .send({
-        email: 'any@mail.com',
-        password: '1validpassword'
-      })
+  test('should successfully sign in with valid user', async () => {
     app.post('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
       .post('/api/signin')
       .send({
-        email: 'any@mail.com',
-        password: '1validpassword'
+        email: validUser.email,
+        password: validUser.password
       })
       .expect(200)
       .then((res) => {
