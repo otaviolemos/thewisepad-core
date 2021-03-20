6a2ec2952ab62c7001902eef1a531ec9bfa77928
Otavio Lemos <otaviolemos@gmail.com>
2021-03-20 12:12:47 -0300

    feat: get requester id for verification

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -5,6 +5,7 @@ export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const request = {
       accessToken: req.headers['x-access-token'],
+      requesterId: req.body.ownerId,
       ...(req.headers || {})
     }
     const httpResponse = await middleware.handle(request)
