485b6179b13a913f0395bbe49802e4d020242206
Otavio Lemos <otaviolemos@gmail.com>
2021-03-21 11:26:51 -0300

    refactor: make a single notes route

--- src/main/routes/notes.ts
+++ src/main/routes/notes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { makeCreateNoteController } from '@/main/factories'
+import { makeCreateNoteController, makeRemoveNoteController } from '@/main/factories'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { authentication } from '@/main/middleware/'
 
 export default (router: Router): void => {
   router.post('/notes', authentication, adaptRoute(makeCreateNoteController()))
+  router.delete('/notes/:noteId', authentication, adaptRoute(makeRemoveNoteController()))
 }
