6543735073dd22a316b9b04c4a7af2de2ac04c19
Otavio Lemos <otaviolemos@gmail.com>
2021-03-22 17:26:32 -0300

    fix: use correct alternative port 5000

--- src/main/server.ts
+++ src/main/server.ts
@@ -5,7 +5,7 @@ require('dotenv').config()
 MongoHelper.connect(process.env.MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
-    const port = process.env.PORT || 500
+    const port = process.env.PORT || 5000
     app.listen(port, () => {
       console.log('Server running at ' + port)
     })
