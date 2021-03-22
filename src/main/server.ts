9adb13ffa67f245c1601f1b4a6a77398dc263bb4
Otavio Lemos <otaviolemos@gmail.com>
2021-03-22 17:17:42 -0300

    feat: server

---
+++ src/main/server.ts
@@ -0,0 +1,13 @@
+import 'module-alias/register'
+import { MongoHelper } from '@/external/repositories/mongodb/helpers/mongo-helper'
+require('dotenv').config()
+
+MongoHelper.connect(process.env.MONGO_URL)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    const port = process.env.PORT || 500
+    app.listen(port, () => {
+      console.log('Server running at ' + port)
+    })
+  })
+  .catch(console.error)
