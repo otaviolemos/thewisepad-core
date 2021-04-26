c9ccfcda38120dc313e99a87a0c6ffe0d8aae0d1
Otavio Lemos <otaviolemos@gmail.com>
2021-04-26 14:50:59 -0300

    feat: set default expiration for jwt to one month

--- src/external/token-manager/jwt-token-manager.ts
+++ src/external/token-manager/jwt-token-manager.ts
@@ -13,7 +13,7 @@ export class JwtTokenManager implements TokenManager {
     if (expires) {
       return jwt.sign(info, this.secret, { expiresIn: expires })
     }
-    return jwt.sign(info, this.secret)
+    return jwt.sign(info, this.secret, { expiresIn: '1m' })
   }
 
   async verify (token: string): Promise<Either<Error, Payload>> {
