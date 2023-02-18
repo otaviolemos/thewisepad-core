919694d6c4460fc8e502d5b3f2b7dbc2e39b3b48
Otavio Lemos <otaviolemos@gmail.com>
2023-02-18 12:24:40 -0300

    fix: file name

--- src/doc/finalize-doc.ts
+++ src/doc/finalize-doc.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export default function finalizeDoc () {
-  const fileName = 'api-dic.json'
+  const fileName = 'api-doc.json'
   const doc = fs.readFileSync(fileName, 'utf8')
   const indexOfLastComma = doc.lastIndexOf(',')
   const docWithoutLastComma = doc.substring(0, indexOfLastComma)
